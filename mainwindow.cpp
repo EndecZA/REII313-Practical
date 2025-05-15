5a7571c49c19ccd376f28df57ba45083835b74cb
Emile van Vuuren <emilevanvuurenimp@gmail.com>
2025-05-15 15:45:28 +0200

    Main Menu v1 design
    Co-Authored-By: STAAL-BURGER <143339631+STAAL-BURGER@users.noreply.github.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,26 +1,52 @@
 #include "mainwindow.h"
 
+#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setFixedSize(800,600);
     setMouseTracking(true);
-    
+
     Background = new QLabel(this);
-  //  setCentralWidget(Background);
-    
+        Background->setGeometry(0, 0, 800, 600);
+        Background->setPixmap(QPixmap("G:/My Drive/North-West University/Year 3/Semester 1/REII313/Practical/Logo.png"));
+        Background->setScaledContents(true); // Scale image to fit label
+        Background->lower(); // Keep behind other widgets
 
     SinglePlayerBtn = new QPushButton(this);
-    SinglePlayerBtn->setFixedSize(700,75);
+    SinglePlayerBtn->setFixedSize(200,75);
     SinglePlayerBtn->setText("Singleplayer");
-    SinglePlayerBtn->move(50,200);
+    SinglePlayerBtn->move(475,300);
+    SinglePlayerBtn->setStyleSheet(
+            "QPushButton {"
+            "   background-color: #FFB347;" // Orange-Yellow
+            "   color: black;" // Dark text for contrast
+            "   border: 1px solid black;" // Soft white border
+            "   border-radius: 5px;"
+            "   font-size: 24px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: #FF8C00;" // Brighter orange on hover
+            "}");
   //  connect(SinglePlayerBtn, SIGNAL(clicked()),this,SLOT(SinglePlayerClicked()));
 
     MultiPlayerBtn = new QPushButton(this);
-    MultiPlayerBtn->setFixedSize(700,75);
+    MultiPlayerBtn->setFixedSize(200,75);
     MultiPlayerBtn->setText("Multiplayer");
-    MultiPlayerBtn->move(50,300);
+    MultiPlayerBtn->move(475,400);
+    MultiPlayerBtn->setStyleSheet(
+            "QPushButton {"
+            "   background-color: #FFB347;"
+            "   color: black;"
+            "   border: 1px solid black;"
+            "   border-radius: 5px;"
+            "   font-size: 24px;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: #FF8C00;" // Brighter orange on hover
+            "}");
   //  connect(MultiPlayerBtn, SIGNAL(clicked()),this,SLOT(MultiPlayerClicked()));
 
 }
