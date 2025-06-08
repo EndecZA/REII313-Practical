662a713c6535ef7aad81d274b58411ddd9acddee
Emile van Vuuren <emilevanvuurenimp@gmail.com>
2025-06-08 19:50:04 +0200

    Completed the switch case for the Enemy Stat blocks

--- enemy.cpp
+++ enemy.cpp
@@ -27,38 +27,47 @@ Enemy::Enemy(EnemyType type, const QPointF& position, QWidget* parent)
         health = 56; damage = 20; walkSpeed = 0.8; attackSpeed = 1.2; attackRange = 1;
         setPixmap(QPixmap(":/resources/images/Orc.png"));
         break;
-
-
-
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
-//        case Wizard:
-//        health = 80; damage = 3; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
-//        setPixmap(QPixmap(":/resources/images/Armored_Skeleton.png"));
-//        break;
+        case Armoured_Orc:
+        health = 128; damage = 20; walkSpeed = 0.8; attackSpeed = 1; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Armored_Orc.png"));
+        break;
+        case Elite_Orc:
+        health = 480; damage = 20; walkSpeed = 1; attackSpeed = 1; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Elite_Orc.png"));
+        break;
+        case Orc_rider:
+        health = 48; damage = 10; walkSpeed = 1.5; attackSpeed = 1; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Orc_Rider.png"));
+        break;
+        case Knight:
+        health = 240; damage = 15; walkSpeed = 0.8; attackSpeed = 0.8; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Knight.png"));
+        break;
+        case Knight_Templar:
+        health = 960; damage = 25; walkSpeed = 0.8; attackSpeed = 0.8; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Knight_Templar.png"));
+        break;
+        case Werebear:
+        health = 280; damage = 20; walkSpeed = 1.5; attackSpeed = 1; attackRange = 1;
+        setPixmap(QPixmap(":/resources/images/Werebear.png"));
+        break;
+        case Cleric:
+        health = 560; damage = 20; walkSpeed = 1; attackSpeed = 1; attackRange = 5;
+        setPixmap(QPixmap(":/resources/images/Cleric.png"));
+        break;
 
     }
+
+    //Definitions for the Animations of the Enemies
+    attackCooldown = 1.0f;
+    lastAttackTime = QTime::currentTime();
+
+    frameWidth = 16;
+    frameHeight = 16;
+    stateFrameCounts[Idle] = 6;
+    stateFrameCounts[Moving] = 6;
+    stateFrameCounts[Attacking] = 6;
+    stateFrameCounts[Dying] = 6;
+
 
 }
